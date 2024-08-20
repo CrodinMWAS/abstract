5a2699ac962f7a749aba3f7a249fcffde0f1a3af
CrodinMWAS <MWASiskola@gmail.com>
2024-08-20 02:49:50 +0200

    HomeFix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView_copy.vue'
+import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import AboutUs from '@/views/AboutUs.vue'
 
